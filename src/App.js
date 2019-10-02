c641db715c956b79a6270b031fc70f939b2fea5d
Marina Blin <marina.blin@zenika.com>
2019-10-02 18:46:54 +0200

    :sparkles: Create input componants

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Switch from './Componants/Switch/switch';
+import Input from './Componants/Input/Input/input';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Switch off='Off' on='On' label='label'></Switch>
+        <Input placeholder='This is placeholder' disabled icon='time'/>
       </header>
     </div>
   );
---
+++ src/Componants/Input/Input/input.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Input as  InputReact} from 'element-react';
+
+const Input = ({ placeholder, disabled, icon }) => {
+    return (
+        <div>
+            <InputReact
+                placeholder={placeholder}
+                disabled={disabled}
+                icon={icon}
+            />
+        </div>
+    )
+}
+
+export default Input;
