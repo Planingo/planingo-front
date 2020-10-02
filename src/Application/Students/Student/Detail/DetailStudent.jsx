eff077553d853e94fa8bee6be5222cc18c2c1614
Marina Blin <m4rina.blin@gmail.com>
2020-10-02 15:56:06 +0200

    :sparkles: Added students details

--- src/Application/Students/Student/Detail/DetailStudent.jsx
+++ src/Application/Students/Student/Detail/DetailStudent.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './detailStudent.scss'
 
-const DetailStudent = () => {
+const DetailStudent = (id, pathway) => {
 	// const id = useParams()
-	// console.log(id)
-	return <div className="details">COUCOU Etudiant</div>
+	console.log(id)
+	return (
+		<div className="details">
+			<div>
+				<img
+					alt="example"
+					src={`https://api.adorable.io/avatars/285/default.png`}
+				/>
+				<p>Nom</p>
+				<p>Prénom</p>
+				<p>Adresse</p>
+				<p>Code postal</p>
+				<p>Date de naissance</p>
+				<p>Formation</p>
+			</div>
+		</div>
+	)
 }
 
 export default DetailStudent
