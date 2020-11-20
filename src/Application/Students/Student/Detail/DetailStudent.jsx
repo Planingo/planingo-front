81740b7c216217eca0825ce585f21932bc2b6f9b
Marina Blin <m4rina.blin@gmail.com>
2020-11-20 23:56:33 +0100

    :sparkles: Added student detail

--- src/Application/Students/Student/Detail/DetailStudent.jsx
+++ src/Application/Students/Student/Detail/DetailStudent.jsx
@@ -1,9 +1,51 @@
+import { last } from 'lodash'
 import React from 'react'
+import { useLocation } from 'react-router'
+import { useGetStudentById } from '../../students.hooks'
+import './detailStudent.scss'
 
 const DetailStudent = () => {
-	// const id = useParams()
-	// console.log(id)
-	return <div className="details">COUCOU Etudiant</div>
+	const location = useLocation()
+
+	const studentId = last(
+		location.pathname.split('/').filter((path) => path !== ''),
+	)
+
+	const { loading, student } = useGetStudentById(studentId)
+
+	if (loading) return null
+
+	return (
+		<div className="details">
+			<div className="student-informations">
+				<img
+					alt="example"
+					src={`https://avatars.bugsyaya.dev/285/${student.id}_${student.pathwayId}`}
+				/>
+				<div>
+					<p className="student-name">
+						{student.firstName} {student.lastName}
+					</p>
+					<div className="align">
+						<p>Formation :</p>
+						<p>{student.pathway.name}</p>
+					</div>
+
+					<div className="align">
+						<p>Apprentissage :</p>
+						<p>
+							{student.apprenticeships.lenght
+								? student.apprenticeships.map(
+										(apprenticeship) => apprenticeship.company.name,
+								  )
+								: '-'}
+						</p>
+					</div>
+				</div>
+			</div>
+			<div className="contraints-informations"></div>
+		</div>
+	)
 }
 
 export default DetailStudent
--- src/Application/Students/students.hooks.js
+++ src/Application/Students/students.hooks.js
@@ -20,6 +20,34 @@ export const useGetAllStudents = () => {
 	return result
 }
 
+export const useGetStudentById = (id) => {
+	const { loading, data } = useQuery(
+		gql`
+			query getStudentById($id: uuid!) {
+				student_by_pk(id: $id) {
+					pathway {
+						name
+						id
+					}
+					firstName
+					id
+					lastName
+					pathwayId
+					apprenticeships {
+						id
+						company {
+							id
+							name
+						}
+					}
+				}
+			}
+		`,
+		{ variables: { id: id } },
+	)
+	return { loading, student: data?.student_by_pk }
+}
+
 export const useAddStudent = () => {
 	const [addStudent, result] = useMutation(
 		gql`
