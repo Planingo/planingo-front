d68f0eead89b98e615ea4fef74005b3e153069f2
Marina Blin <m4rina.blin@gmail.com>
2020-11-20 13:51:09 +0100

    :sparkles: Added header for details

--- src/Components/Refinement/refinement.jsx
+++ src/Components/Refinement/refinement.jsx
@@ -1,43 +1,74 @@
-import { CalendarOutlined } from '@ant-design/icons'
+import { ArrowLeftOutlined, CalendarOutlined } from '@ant-design/icons'
 import { Radio } from 'antd'
 import React from 'react'
 import { useIntl } from 'react-intl'
+import { Link } from 'react-router-dom'
 import AddCalendar from '../../Application/Calendars/Calendar/Add/AddCalendar'
+import AddItem from '../../Application/Layout/Add/AddItem'
 
 const Refinement = ({
 	options,
 	setIsGrid,
 	isGrid,
-	AddItem,
-	FirstIcon,
-	firstAddText,
-	Form,
-	adding,
-	onAdd,
+	backTo,
+	FirstActionItem,
+	FirstActionIcon,
+	firstActionText,
+	FirstForm,
+	firstActioning,
+	onFirstAction,
+	SecondActionItem,
+	SecondActionIcon,
+	secondActionText,
+	SecondForm,
+	secondActioning,
+	onSecondAction,
 }) => {
 	const intl = useIntl()
 
 	return (
 		<div className="refinement">
-			<Radio.Group
-				options={options}
-				onChange={() => setIsGrid(!isGrid)}
-				value={isGrid ? 'Grille' : 'List'}
-				optionType="button"
-				buttonStyle="solid"
-			/>
+			{options ? (
+				<Radio.Group
+					options={options}
+					onChange={() => setIsGrid(!isGrid)}
+					value={isGrid ? 'Grille' : 'List'}
+					optionType="button"
+					buttonStyle="solid"
+				/>
+			) : (
+				<Link to={`/${backTo}`}>
+					<ArrowLeftOutlined />
+				</Link>
+			)}
 			<div className="refinement-item">
-				<AddItem
+				<FirstActionItem
 					title={
 						<div>
-							<FirstIcon />
-							<p>{firstAddText}</p>
+							<FirstActionIcon />
+							<p>{firstActionText}</p>
 						</div>
 					}
-					Form={Form}
-					adding={adding}
-					onAdd={onAdd}
+					Form={FirstForm}
+					adding={firstActioning}
+					onAdd={onFirstAction}
 				/>
+
+				{secondActionText ? (
+					<SecondActionItem
+						title={
+							<div>
+								<SecondActionIcon />
+								<p>{secondActionText}</p>
+							</div>
+						}
+						Form={SecondForm}
+						adding={secondActioning}
+						onAdd={onSecondAction}
+					/>
+				) : (
+					<></>
+				)}
 
 				<AddItem
 					secondary
