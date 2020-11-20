40c92b2815ddb00e4e1d0017d0446a79d16d4bdd
Marina Blin <Bugsyaya@users.noreply.github.com>
2020-11-21 00:01:07 +0100

    :sparkles: Added student detail (#46)

--- src/Application/Pathways/pathways.hooks.js
+++ src/Application/Pathways/pathways.hooks.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
+import { isNil } from 'lodash'
 
 export const useGetAllPathways = () => {
 	const result = useQuery(gql`
@@ -12,4 +13,19 @@ export const useGetAllPathways = () => {
 		}
 	`)
 	return result
+}
+
+export const useGetPathwayById = (id) => {
+	const { loading, data } = useQuery(
+		gql`
+			query getPathwayById($id: uuid!) {
+				pathway_by_pk(id: $id) {
+					id
+					name
+				}
+			}
+		`,
+		{ variables: { id: id }, skip: isNil(id) },
+	)
+	return { loadingPathway: loading, pathway: data?.pathway_by_pk }
 }
