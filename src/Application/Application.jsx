f9555f88d29a7b4e77fd26b16c8fa6caed9962af
Marina Blin <m4rina.blin@gmail.com>
2021-04-17 23:48:14 +0200

    Correctif settings

--- src/Application/Application.jsx
+++ src/Application/Application.jsx
@@ -5,6 +5,7 @@ import Navigation from './Navigation/Navigation'
 import {Student, Professor, Calendar, Lesson, Room, Company} from './Routes'
 import { Pathway } from './Routes/Pathway'
 import { Module } from './Routes/Module'
+import Settings from './Settings/Settings'
 
 const Application = () => {
 
@@ -38,51 +39,9 @@ const Application = () => {
 						<Route path="/companies">
 							<Company/>
 						</Route>
-						{/* 
-						<Route path="/companies/:id">
-							<Refinement
-								backTo="companies"
-								FirstActionIcon={WifiOutlined}
-								firstActionText={intl.formatMessage({ id: 'edit.company' })}
-								FirstForm={AddCompany}
-								onFirstAction={addCompany}
-								firstActioning={addingCompany}
-								SecondActionIcon={EditOutlined}
-								secondActionText={intl.formatMessage({
-									id: 'edit.constraints',
-								})}
-								SecondForm={AddCompany}
-								onSecondAction={editCompany}
-								secondActioning={editingCompany}
-							/>
-							<DetailCompany />
-						</Route>
-						<Route path="/companies">
-							<div className="header">
-								<Search
-									placeholder="Rechercher une entreprise"
-									onSearch={onCompanySearch}
-								/>
-								<Refinement
-									options={options}
-									setIsGrid={setIsGrid}
-									isGrid={isGrid}
-									FirstActionIcon={WifiOutlined}
-									firstActionText={intl.formatMessage({ id: 'add.company' })}
-									FirstForm={AddCompany}
-									onFirstAction={addCompany}
-									firstActioning={addingCompany}
-								/>
-							</div>
-							{!isGrid ? (
-								<CompaniesList companySearch={companySearch} />
-							) : (
-								<Companies companySearch={companySearch} />
-							)}
-						</Route>
 						<Route path="/settings">
 							<Settings />
-						</Route> */}
+						</Route>
 					</Switch>
 				</div>
 			</div>
